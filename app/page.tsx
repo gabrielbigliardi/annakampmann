6aaef907533c0c73124b1f3ad75a2a1abefefd2e
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2024-03-05 14:52:27 -0300

    hero mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,37 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="z-10 ">
-      <section className="relative h-96 bg-red-300">
+      <section >
+        <div className="absolute gradient ">
+          <div className="relative p-7 w-2/5 top-[13rem]">
+
+            <h1 className="h1">Dra. Anna Kampmann</h1>
+            <h2 className="description">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos obcaecati laudantium asperiores molestias necessitatibus alias provident.
+            </h2>
+          </div>
+        </div>
+
         <Image
-          src={"/anna_hero.png"}
-          alt="Anna Kampmann"
-          className="left-0"
-          fill
-          style={{ objectFit: "cover" }}
-        ></Image>
+          src={"/logo_hero_2.png"}
+          alt="Anna Logo"
+          className="absolute opacity-5"
+          width={782}
+          height={1127}
+        />
+
+        <div className="float-right">
+          <Image
+            src={"/anna_hero.png"}
+            alt="Anna Kampmann"
+            className=" right-0 opacity-15 bg-blue-300"
+            height={976}
+            width={976}
+          />
+        </div>
+
+
+
       </section>
     </main>
   );
