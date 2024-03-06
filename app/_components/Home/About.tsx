139aa83b16e819d9a43bf9a1bb7940fca162a0f6
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2024-03-06 00:25:51 -0300

    home about

--- app/_components/Home/About.tsx
+++ app/_components/Home/About.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function About() {
     return (
         <section className='bg-boxes'>
-            <div className='h-[921px]'>
-                <div className='relative w-screen h-[250px] '>
+            <div className='about'>
+                <div className='relative w-screen h-[250px] about-1 '>
                     <Image
                         src="/about_1.jpg"
                         alt="Anna Service"
@@ -22,14 +23,25 @@ export default function About() {
                     />
                 </div>
 
-                <div className='relative px-8 pt-36 text-text'>
-                    <h2 className='h2'>Sobre</h2>
-                    <p className='paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum id tortor sit amet euismod. Etiam ultricies, nibh nec tempor fermentum, massa ligula fringilla eros, nec porta justo eros et elit. Nullam malesuada accumsan maximus. Nunc et libero quis justo vehicula porttitor vel nec urna. Suspendisse pulvinar, magna sed consectetur convallis, turpis lectus aliquet nisl, ut efficitur dui turpis at nibh. Donec scelerisque vel massa non maximus. In ut elementum diam. In nec posuere mi. Nullam posuere, mi vitae sollicitudin convallis, sapien nibh mattis elit, ac ultrices dolor orci et orci. Fusce quis malesuada turpis.</p>
+                <div className='about-images-desktop-container'>
+                    <Image
+                        src={'/about-images-desktop.png'}
+                        alt='About Anna'
+                        width={750}
+                        height={750}
+                        className='about-image-desktop'
+                    // fill
+                    // objectFit='contain'
+                    />
                 </div>
 
-                <div className='about-2'
-                // style={{ marginRight: '-50%' }}
-                >
+                <div className='about-text-desktop'>
+                    <h2 className='h2'>Sobre</h2>
+                    <p className='paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum id tortor sit amet euismod. Etiam ultricies, nibh nec tempor fermentum, massa ligula fringilla eros, nec porta justo eros et elit. Nullam malesuada accumsan maximus. Nunc et libero quis justo vehicula porttitor vel nec urna. Suspendisse pulvinar, magna sed consectetur convallis, turpis lectus aliquet nisl, ut efficitur dui turpis at nibh. Donec scelerisque vel massa non maximus. In ut elementum diam. In nec posuere mi. Nullam posuere, mi vitae sollicitudin convallis, sapien nibh mattis elit, ac ultrices dolor orci et orci. Fusce quis malesuada turpis.</p>
+                    <Link href={'/about'} className='px-10 py-1 text-whit bg-text'>Ver mais</Link>
+                </div>
+
+                <div className='about-2 '>
                     <Image
                         src={'/dots.png'}
                         alt='Dots'
