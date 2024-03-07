b805bdd633091f9b2b1699a04e91574309d38a8c
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2024-03-07 17:42:47 -0300

    feat: schedule

--- app/(home)/_components/Schedule.tsx
+++ app/(home)/_components/Schedule.tsx
@@ -1,9 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Schedule() {
     return (
-        <div className='HOME-schedule'>
+        <div className='HOME-schedule shadow'>
+            <Image
+                src={'/logo_schedule.png'}
+                alt='Logo'
+                fill
+                className='HOME-schedule-logo'
+            />
 
+            <Image
+                src={'/roro.png'}
+                alt='Aurora Maria'
+                fill
+                className='HOME-schedule-roro'
+            />
+
+            <div className='HOME-schedule-info'>
+                <h2>Marque uma consulta pelo nosso WhatsApp</h2>
+                <Link href={'/'} className='button'>Clique Aqui</Link>
+            </div>
         </div>
     )
 }
