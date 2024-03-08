eb001ab4e413ddafa303c1857e77576f1aee9766
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2024-03-08 15:27:41 -0300

    fix: navbar close

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 import { Menu, X } from 'lucide-react';
 import Image from 'next/image'
 import React, { useState } from 'react'
@@ -6,9 +6,19 @@ import Link from 'next/link';
 
 export default function Header() {
 
+  const [isChecked, setIsChecked] = useState(false)
+
+  function closeMenu() {
+    //TODO: USAR STATE
+
+    // document.getElementById('sidebar-active')?.checked = false
+
+
+  }
+
   return (
     <nav className='shadow'>
-      <input type="checkbox" id='sidebar-active' />
+      <input type="checkbox" id='sidebar-active' checked={isChecked} />
 
       <Image
         src={'/logo_header.png'}
@@ -16,21 +26,29 @@ export default function Header() {
         width={225}
         height={45}
       />
-      <label htmlFor="sidebar-active" className='open-sidebar-button'>
+      <label
+        htmlFor="sidebar-active"
+        className='open-sidebar-button'
+        onClick={() => setIsChecked(true)}
+      >
         <Menu />
       </label>
 
       <div className="links-container">
 
-        <label htmlFor="sidebar-active" className='close-sidebar-button'>
+        <label
+          htmlFor="sidebar-active"
+          className='close-sidebar-button'
+          onClick={() => setIsChecked(false)}
+        >
           <X />
         </label>
 
         <span>MENU</span>
-        <Link href={'/'}>Início</Link>
-        <Link href={'/sobre'}>Sobre</Link>
-        <Link href={'/clinica'}>Clínica</Link>
-        <Link href={'/procedimentos'}>Procedimentos</Link>
+        <Link href={'/'} onClick={() => setIsChecked(false)}>Início</Link>
+        <Link href={'/sobre'} onClick={() => setIsChecked(false)}>Sobre</Link>
+        <Link href={'/clinica'} onClick={() => setIsChecked(false)}>Clínica</Link>
+        <Link href={'/procedimentos'} onClick={() => setIsChecked(false)}>Procedimentos</Link>
         <Link href={'/'} className='nav-schedule-button'>Marque atendimento</Link>
 
         <Image
